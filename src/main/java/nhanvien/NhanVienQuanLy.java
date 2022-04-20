8571a7c45ee1f889c41d53272db5b23281107f3d
anhnguyenvan0910@gmail.com <anhnguyenvan0910@gmail.com>
2022-04-20 17:16:36 +0700

    Update file

--- src/main/java/nhanvien/NhanVienQuanLy.java
+++ src/main/java/nhanvien/NhanVienQuanLy.java
@@ -1,4 +1,22 @@
 package nhanvien;
 
-public class NhanVienQuanLy {
+import phongban.PhongBan;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class NhanVienQuanLy extends NhanVien {
+    private final List<Calendar> ngayNhamChuc = new ArrayList<>();
+    private final List<PhongBan> phongBanQuanLy = new ArrayList<>();
+
+    public NhanVienQuanLy(int maNhanVien, String hoTen, String email, String gioiTinh, PhongBan phongBan,
+                              double heSoLuong, double luongCoBan) {
+        super(maNhanVien, hoTen, email, gioiTinh, phongBan, heSoLuong, luongCoBan);
+    }
+
+    @Override
+    public double tinhLuong() {
+        return super.getHeSoLuong() * super.getLuongCoBan();
+    }
 }
