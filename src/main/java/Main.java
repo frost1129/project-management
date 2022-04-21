ec91e968b3d29b2b3b31ecb51b4f23f3d8520b92
anhnguyenvan0910@gmail.com <anhnguyenvan0910@gmail.com>
2022-04-21 17:25:10 +0700

    Update file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,23 @@ public class Main {
     //Hàm in menu các chức năng của nhân viên
     public static void menuNhanVien () {
         System.out.print("--- CHỨC NĂNG NHÂN VIÊN ---" +
-                "\n1. Thêm 1 nhân viên (bình thường)" +
-                "\n2. Thêm 1 nhân viên (lập trình viên)" +
-                "\n3. Thêm 1 nhân viên (kiểm thử viên)" +
-                "\n4. Thêm 1 nhân viên (thiết kế viên)" +
-                "\n5. Xem danh sách dự án tham gia của 1 nhân viên" +
-                "\n6. Tìm kiếm nhân viên theo họ tên, ngày sinh, theo phòng ban" +
+                "\n1. Thêm 1 nhân viên" +
+                "\n2. Xem danh sách dự án tham gia của 1 nhân viên" +
+                "\n3. Tìm kiếm nhân viên theo họ tên, ngày sinh, theo phòng ban" +
+                "\n4. Tính lương cho 1 nhân viên" +
+                "\nNhập lựa chọn (0 - 6): ");
+    }
+    //Hàm in menu các chức năng của dự án
+    public static void menuDuAn () {
+        System.out.print("--- CHỨC NĂNG DỰ ÁN ---" +
+                "\n1. Thêm 1 dự án" +
+                "\n2. Sửa 1 dự án" +
+                "\n3. Xóa 1 dự án" +
+                "\n3. Xem danh sách nhân viên của 1 dự án" +
+                "\n4. Tìm kiếm dự án theo tên và thời điểm bắt đầu" +
+                "\n5. Sắp xếp dự án theo kinh phí đầu tư" +
+                "\n6. Gán nhân viên cho dự án" +
+                "\n7. Gán người quản lý cho dự án" +
                 "\nNhập lựa chọn (0 - 6): ");
     }
 }
